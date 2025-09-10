eacebcab775f071878a0b5a3a1b48c25a9c40e90
minjumsumath <minjum.ara.sumath@g.bracu.ac.bd>
2025-09-10 20:11:22 +0600

    authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -81,6 +81,9 @@ export const login = async (req, res) => {
     res
       .cookie("accessToken", token, {
         httpOnly: true,
+        sameSite: "lax",
+        secure: false,
+        path: "/",
         expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000), // 15 days
       })
       .status(200)
